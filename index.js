261d465b50a788fdfa267735eaef1ddf2a8e1da8
ksnxr <random_username_1@outlook.com>
2021-06-01 00:27:33 +0800

    Finish 3.18.

--- index.js
+++ index.js
@@ -18,50 +18,95 @@ morgan.token('body', (request, response) => {
         return ''
     }
 })
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people\n${new Date().toString()}`)
+    Person.estimatedDocumentCount()
+        .then(count => {
+            response.send(`Phonebook has info for ${count} people\n${new Date().toString()}`)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(result => {
-        response.json(result)
-    })
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+    Person.findById(request.params.id)
+        .then(foundPerson => {
+            response.json(foundPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id).then(person => {
-        response.status(204).end()
-    })
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const getRandomInt = max => Math.floor(Math.random() * max)
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (body.name === undefined) {
-        return response.status(400).json({ error: 'name missing' })
-    } else if (body.number === undefined) {
-        return response.status(400).json({ error: 'number missing' })
-    }
+    // if (body.name === undefined) {
+    //     return response.status(400).json({ error: 'name missing' })
+    // } else if (body.number === undefined) {
+    //     return response.status(400).json({ error: 'number missing' })
+    // }
 
     const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
